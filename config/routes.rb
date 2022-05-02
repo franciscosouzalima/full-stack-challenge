3e6e971828ee749366a11808d3c3b2541939fbb5
Francisco Lima <franciscosouzalima@gmail.com>
2022-05-02 18:49:43 -0300

    add name to user, add author to book, add list users

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
+  get 'users/index'
   root to: 'lends#index'
 
   resources :lends
   resources :books
   devise_for :users
+
+  match '/users',   to: 'users#index',   via: 'get'
 
   devise_scope :user do
     authenticated :user do
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_02_191559) do
+ActiveRecord::Schema.define(version: 2022_05_02_204412) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2022_05_02_191559) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "author"
   end
 
   create_table "lends", force: :cascade do |t|
@@ -39,6 +40,7 @@ ActiveRecord::Schema.define(version: 2022_05_02_191559) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "admin", default: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
